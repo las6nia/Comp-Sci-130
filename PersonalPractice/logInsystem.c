255626e43d191222eedd6784b98232fa683133eb
las6nia <lalcalaroblero2@gmail.com>
2025-08-22 18:13:38 -0500

    Update logInsystem.c
    Lots of bugs, knowledge of C (so far)

--- PersonalPractice/logInsystem.c
+++ PersonalPractice/logInsystem.c
@@ -1,1 +1,45 @@
-// this is a test
+#include <stdio.h>
+
+int main(void)
+{
+    char name[15];
+    char deny[2];
+    char created_username;
+
+    char username = "Las6nia";
+    char password = "P@piluis201";
+
+    printf("Enter Username, otherwise\n");
+    printf("if you don't have one, enter 'n': \n");
+    scanf("%14s", &username);
+    scanf("%c", &deny);
+
+    if (deny == 'n' || deny == 'N')
+    {
+
+        char new_username[20];
+        char new_password[30];
+
+        printf("Create a username: \n");
+        scanf("%19s", &new_username);
+        printf("Great! Your username is %19s\n", new_username);
+        Printf("Create a password: ");
+        scanf("%30s", new_password);
+        printf("Great! Now your password is %30s", new_password);
+    }
+
+    else
+    {
+
+        char enter_username = printf("Enter your username: ");
+
+        if (enter_username != username)
+        {
+            printf("Wrong username, Goodbye\n"); // try to do a loop to continue to try again
+        }
+        else
+        {
+            printf("Success!")
+        }
+    }
+}
