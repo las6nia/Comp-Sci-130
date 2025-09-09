e218cd957f55cb780a8c3509563d4b16c5a8ada1
las6nia <lalcalaroblero2@gmail.com>
2025-09-09 13:22:20 -0500

    Create Mainclass.java

---
+++ java/Mainclass.java
@@ -0,0 +1,30 @@
+package instructor1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.print("Hello\t");
+		//System.out.print("UWGB");
+		
+		//System.out.print("Wisconsin\tGreen Bay\n");
+		//System.out.print("Madison\tAppleton\n");
+		
+		//System.out.print("Wisconsin\n\tGreen Bay\n\t\tMadison");
+		//Note: the above three statements could be combined in a single statement
+		
+		//System.out.print("Java\n");
+		//the above statement can be rewritten by println command as following
+		//System.out.println("Java");
+		
+		//rewrite S1 using println
+		System.out.println("Wisconsin\n\tGreen Bay\n\t\tMadison");
+		
+		System.out.print("UWGB\n");
+		System.out.println("UWGB");
+		
+		
+		
+	}
+
+}
