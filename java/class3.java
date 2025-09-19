4c9a9c216faf38dbb7848fe060b190b75c3ed80b
las6nia <lalcalaroblero2@gmail.com>
2025-09-18 21:18:48 -0500

    Create class3.java

---
+++ java/class3.java
@@ -0,0 +1,120 @@
+package Class3;
+import java.util.Scanner; //import library
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+/*
+		//EXAMPLE - PART 2
+							//TASK 2 - RECEIPT//
+		//image saved on sept 16 at 1:20pm 
+		
+		double subTotal = 0.0;
+		double total = 0.0;
+		//total that john should pay
+		System.out.println("\t\t\tCulver's, Green Bay, Wi");
+		System.out.println("1 ButterBurger Basket -------------- $12.22");
+		subTotal = subTotal + 12.22; //remember to initialize subTotal to = 0.0
+		System.out.printf("\t\t\t Sub Total = $%.2f\n",subTotal);
+		
+		System.out.println("2 Kids Meals ------------------------ $7.12");
+		
+		subTotal = subTotal + (7.12)*2; //remember to initialize subTotal to = 0.0
+		System.out.printf("\t\t\t Sub Total = $%.2f\n",subTotal);
+		System.out.println("Coupon Discount --------------------- $2.00");
+		subTotal = subTotal - 2; //remember to initialize subTotal to = 0.0
+		System.out.printf("\t\t\t Sub Total = $%.2f\n",subTotal);
+		
+		System.out.println("Wisconsin Tax --------------------- %5.5");
+		total= subTotal * 1.05; // OR subTotal * .05 // this is written as subTotal * 1.05;
+		
+		System.out.printf("\t\t\t     Total = $%.2f\n",total);
+		
+
+*/
+		
+
+		
+		
+
+		
+		
+/*			          *******STUDY*******
+		int x = 2;
+		//consider below statement
+		x = x+2;
+		//updating value of X
+		System.out.print(x);
+
+		//Note:
+		// X++ (post)
+		//++X (Pre)
+		
+		//increment
+		x+=1; // x++ or ++x - Increment
+		x-=1; // X-- or --X - Decrement
+		
+		
+		//Re-writing
+		 x = x+4; //is equal to x+=4
+		
+*/
+		
+		
+		
+		
+/*					//TASK 1: 
+ 		//create a scanner object
+ 			Scanner input = new Scanner(System.in);
+		// 7/4 in math = 1 and remainder is 3
+		//in Java - 7/4 = int/int = 1
+		//7%4 = 3 (the percentage will give you the remainder)
+		
+		//dollars = 120/100 = 1 dollars
+		//cents = 120%100 = 20 cents
+		//int dollars
+		//int cents
+		
+							//TASK:
+		//EXERCISE #4 - turning 500 into 8 minutes and 20 seconds.
+		
+		//real coding
+		int totalSeconds; // this is our ONLY input
+		
+		int minutes;
+		int seconds;	//2 outputs
+		int hours;
+	
+		//step 1: get input from the user, be aware that the type is an interger
+		System.out.print("Enter an number for seconds: "); //as shown in the given TEST CASE(sample run). STUDY
+		totalSeconds = input.nextInt();
+		
+		//Step 2: Solve for minutes and seconds from input value (TotalSeconds)
+		minutes = totalSeconds / 60; //int /int
+		seconds = totalSeconds % 60;
+		
+		
+		hours = minutes/60; //for fun?? PRACTICE
+		
+		//Step 3: display the output
+		System.out.printf("%d seconds is %d minutes and %d seconds, and %d hours",totalSeconds,minutes,seconds,hours);
+		
+		//Step 4 = output 
+		//Enter an number for seconds: 500
+		//500 seconds is 8 minutes and 20 seconds
+		
+	
+		
+		
+		//Q - CAN WE APPLY % WITH DOUBLE VALUES?
+		System.out.println(4.78 % 3.25); //double/double
+		//yes, we can use modular with float, int, double
+*/
+		
+		
+		
+
+	}
+
+}
