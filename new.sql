15b3e561f1a44c271c380fc9c8f83dc5b5440e34
las6nia <lalcalaroblero2@gmail.com>
2025-09-23 20:49:05 -0500

    Update new.sql

--- new.sql
+++ new.sql
@@ -1,7 +1,1 @@
--- This is an important query used later to join with the accounts table 
-SELECT
- 		rowkey,  -- key used to join with account_id
-Info.date,  -- date is in string format YYYY-MM-DD HH:MM:SS
-Info.code  -- e.g., 'pub-###'
 
-FROM  Publishers
