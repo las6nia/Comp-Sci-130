11f10b0282abd403b019455380b22272ff95e8a4
las6nia <lalcalaroblero2@gmail.com>
2025-10-20 20:25:02 -0500

    Update java lab 3

--- lab3.java
+++ lab3.java
@@ -1,5 +1,3 @@
-/*FIX ERRORS*/
-
 package Labs;
 import java.util.Scanner;
 
@@ -8,41 +6,107 @@ public class Lab3 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		String major;
-		
-		int freshman = 1; int sophmore = 2; int junior = 3; int senior = 4;
+		String main;
+		char major;
 		int year;
+		String majorTest;
 		
-		System.out.print("Enter your major: ");
-		major = input.nextLine().toLowerCase();
-		
-		System.out.print("Enter your year: ");
-		year = input.nextInt();
-		
-		if ((major.charAt(0) == 'm'))
-		{
-			if (year >= freshman || year < sophmore) {
-				System.out.println("Hello");
+		System.out.println("enter 2 characters: ");
+		main = input.nextLine();
 
+		major = main.toLowerCase().charAt(0);
+		year = Integer.parseInt(main.substring(1));
+//System.out.printf("%c and %d",major,year+1); - testing
+		
+	
+		if (major == 'm'){
+			switch (year) {
+			case 1:
+			{
+				System.out.print("Hello, Mathematics Freshman");
+				break;
 			}
-			else if (year > freshman || year < junior) {
-				System.out.println("Hello Mathematics Sophmore");
-				}
-			else if (year > sophmore|| year < senior) {
-				System.out.println("Hello Mathematics Junior");
-				}
-			else {
-				System.out.println("Hello Mathematics Senior");
+			case 2:
+			{
+				System.out.print("Hello, Mathematics Sophmore");
+				break;
 			}
+			case 3:
+			{
+				System.out.print("Hello, Mathematics Junior");
+				break;
+			}
+			case 4:
+			{
+				System.out.print("Hello, Mathematics Senior");
+				break;
+			}
+			default:
+				System.out.println("Invalid input");
+				
 		}
-		else if ((major.charAt(0) == 'c')) {
-			if (year >= freshman || year < sophmore) {
-				System.out.println("Hello Computer Science Freshman");
-		}
-			
-		
-		
-		
 	}
+		else if (major == 'c'){
+			switch (year) {
+			case 1:
+			{
+				System.out.print("Hello, Computer Science Freshman");
+				break;
+			}
+			case 2:
+			{
+				System.out.print("Hello, Computer Science Sophmore");
+				break;
+			}
+			case 3:
+			{
+				System.out.print("Hello, Computer Science Junior");
+				break;
+			}
+			case 4:
+			{
+				System.out.print("Hello, Computer Science Senior");
+				break;
+			}
+			default:
+				System.out.println("Invalid input");
+		}
+	}
+		else if (major == 'i'){
+			switch (year) {
+			case 1:
+			{
+				System.out.print("Hello, Information Science Freshman");
+				break;
+			}
+			case 2:
+			{
+				System.out.print("Hello, Information Science Sophmore");
+				break;
+			}
+			case 3:
+			{
+				System.out.print("Hello, Information Science Junior");
+				break;
+			}
+			case 4:
+			{
+				System.out.print("Hello, Information Science Senior");
+				break;
+			}
+			default:
+				System.out.println("Invalid input");
+				
+			
+		}
+	}
+		
+		else {
+			System.out.println("Invalid input");
 
+		}
+		
+		
+	
+	}
 }
