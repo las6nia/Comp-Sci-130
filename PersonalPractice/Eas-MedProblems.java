80d704ed8ca675f0f913dc9c4089184e227c3471
Luis Alcala <lalcalaroblero2@gmail.com>
2025-10-22 17:53:13 -0500

    Create Eas-MedProblems.java

---
+++ PersonalPractice/Eas-MedProblems.java
@@ -0,0 +1,5 @@
+//🧩 Problem 1 – Even or Odd Write a Java program that asks the user for an integer and prints whether it’s even or odd. Example: Enter a number: 9 9 is odd
+
+//🔢 Problem 2 – Sum of First N Numbers Ask the user for a positive integer n, and print the sum of all numbers from 1 to n. Example: Enter a positive integer: 5 The sum is 15 (Hint: you can use a for loop or the formula n*(n+1)/2.)
+
+//💬 Problem 3 – Count Vowels Ask the user to enter a word and count how many vowels (a, e, i, o, u) are in it. Example: Enter a word: Java There are 2 vowels.
