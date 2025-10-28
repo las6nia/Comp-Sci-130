4f3d00aa5d13f9486c1dc01606cbe94bbc52bf21
Luis Alcala <lalcalaroblero2@gmail.com>
2025-10-27 23:27:32 -0500

    Refactor coin sorting logic and improve readability
    Second Update - whoops!

--- lab4Windows.java
+++ lab4Windows.java
@@ -6,82 +6,69 @@ public class main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		//Lab 4 - Sentinel- controlled iterations
-		//Coin sorter
+		//Coin sorter	
 		
+		//Good things - passes case 1 and 2 but not case 3
 		String coins;
 		double q = .25; //quarter
 		double d = .10; //dime
 		double n = .05; //nickle
-		double p = 0.1; //penny
-		
+		double p = 0.01; //penny
+
 		int qCounter = 0;
 		int dCounter = 0;
 		int nCounter = 0;
 		int pCounter = 0;
 		
+		int undefinedCoin = 0;
 		int counter = 0;
 		
-		System.out.print("Enter the coin code:\n");
+
+		System.out.print("Enter the coin code: \n");
 		coins = input.nextLine();
-		coins = coins.toLowerCase();
+		coins = coins.trim().toLowerCase(); //should I add trim to remove spaces??
 		int index =0;
 		int undefinedCoins= 0;
 		
 	while(index <= coins.length()-1) {
 		char copy = coins.charAt(index);
-		if (index == copy){
-			while (index>=0) {
-				if (copy == 'q') {
-				System.out.print("Q test");
-				q+=q;
+		switch (copy) {
+			case ('q'):
 				qCounter++;
+				break;
+			case ('d'):
+				dCounter++;
+				break;
+			case ('n'):
+				nCounter++;
+				break;
+			case ('p'):
+				pCounter++;
+				break;
+			case ('x'):
+				break;
+			default:	
+				undefinedCoins++;
+				break;
 				}
-				else if (copy == 'd') {
-					System.out.print("d Test");
-					d+=d;
-					dCounter++;
-					}
-				else if (copy == 'n') {
-					System.out.print("n Test");
-					n+=n;
-					nCounter++;
-					}
-				else if (copy == 'p') {
-					System.out.print("p Test");
-					p+=p;
-					pCounter++;
-					}
-				else {
-					System.out.print("Enter a valid Syntax");
-					break;
-				}
-			}
-			
-		}
-		else {
-			System.out.print("Enter a valid syntax");
-			undefinedCoins+=undefinedCoins;
-			continue;
-		}
 		
-		counter++;
-		System.out.print("Enter the coin code:\n");
-		coins = input.nextLine();
+		index++;
+	}
+
+		double add = (qCounter*q)+(nCounter*n)+(dCounter*d)+(pCounter*p);
+
+
+		System.out.printf("Coin counts:\n");
+		System.out.print("-".repeat(10));
+		System.out.printf("\nPennies: %d",pCounter);
+		System.out.printf("\nNickles: %d",nCounter);
+		System.out.printf("\nDimes: %d",dCounter);
+		System.out.printf("\nQuarters: %d\n",qCounter);
+		System.out.println("-".repeat(10));
+		System.out.printf("TOTAL VALUE: $%.2f",add); //,add
+		System.out.printf("\nNumber of unidentified coins: %d",undefinedCoins); //,undefinedCoins);
 		
-		
-		}
-	double add = (qCounter*q)+(nCounter*n)+(dCounter*d)+(pCounter*p);
 	
-		System.out.printf("\nCoin counts:\n");
-		System.out.print('-'*5);
-		System.out.printf("\nPennies: %d",p);
-		System.out.printf("\nNickles: %d",n);
-		System.out.printf("\nDimes: %d",d);
-		System.out.printf("\nQuarters: %d\n",q);
-		System.out.println('-'*5);
-		System.out.printf("TOTAL VALUE: %.2f", add);
-		System.out.printf("Number of unidentified coins: %.2f", undefinedCoins);
-		
 	}
 
 }
