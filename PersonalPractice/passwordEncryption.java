3533daeef9a0202981876be3074702318f0b9b59
Luis Alcala <lalcalaroblero2@gmail.com>
2025-11-11 11:56:15 -0600

    Create passwordEncryption.java

---
+++ PersonalPractice/passwordEncryption.java
@@ -0,0 +1,32 @@
+package passwordEncryption;
+import java.util.Scanner;
+
+public class MainEncryption {
+	public static Scanner input = new Scanner(System.in);
+
+	//inpiration-  https://www.youtube.com/watch?v=Sv8WnGj1D6c
+	public static void main(String[] args) {
+		
+		
+		System.out.print("Enter how many digits in your password: ");
+		int digits = input.nextInt();
+		
+		System.out.print("Would you like a mix of symbols and numbers? [Y/N]: ");
+		char mix= input.nextLine().toLowerCase().charAt(0);
+		
+		
+		while (digits>0 && mix == 'y' || mix == 'n') {
+			boolean processing =true;
+				String generatedPassword;
+				
+				if ()
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
