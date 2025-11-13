a6861f1b6e8464e49459c03885cc044627b49566
Luis Alcala <lalcalaroblero2@gmail.com>
2025-11-13 13:30:28 -0600

    Create class19DataStructures.java

---
+++ java/class19DataStructures.java
@@ -0,0 +1,19 @@
+package lecture19NextChapter;
+
+public class OneDimentionalArrays {
+
+	public static void main(String[] args) {
+		//Chapter 7 - Part 1: One dimensional arrays
+		//Developing data structures, warehouses
+						//Part 1 - linear data structures, part 2 - Tables, matrix, part 3 - Dynamic structures
+		//Data structures, store data and retrieve data from lists and tables of value
+		//Use methods to work with arrays  (warehouses), they have a requirement, same data type
+		//Values, all int, or double, strings, char,  - you cannot have a mix of values in the warehouse (SAME DATA TYPE)
+		
+											//PART 1 - LINEAR DATA STRUCTURES
+		
+		
+
+	}
+
+}
