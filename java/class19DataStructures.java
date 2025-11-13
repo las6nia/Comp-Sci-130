bf110139173561e761ed032ff550875838cff02e
Luis Alcala <lalcalaroblero2@gmail.com>
2025-11-13 13:40:33 -0600

    Update class19DataStructures.java

--- java/class19DataStructures.java
+++ java/class19DataStructures.java
@@ -11,7 +11,22 @@ public class OneDimentionalArrays {
 		//Values, all int, or double, strings, char,  - you cannot have a mix of values in the warehouse (SAME DATA TYPE)
 		
 											//PART 1 - LINEAR DATA STRUCTURES
+		//Question, How can we create an array in languages? 
+		//Answer = 2 ways, 					you need the 
+					//		Create an empty array = type [] array'sName = newType[size] = 
+								//each array has the following = type, name, size.
+						//The type of elements that will be stored in this array
+					//size - the maximum number of elements that I can store in this array, known as length, capacity
+		//........................................................................................................................
+		//Task = create an array named AA that can store up to 10 integers
+		int [] AA = new int [10];
+	//Note - in each array is considered as a set or a line of spots or rooms where each room can store only a single element
+	//We don't say value, we element
+		//Each room has an index, you have 10 rooms, starting from 0 - 9 () 
+//Task - create an array with 5 doubles called BB
+		double [] BB = new double [5];
 		
+					
 		
 
 	}
