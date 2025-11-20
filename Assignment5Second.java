d26d613ed25f7f7bd69bda124f80d087fc349719
Luis Alcala <lalcalaroblero2@gmail.com>
2025-11-20 14:27:55 -0600

    Improve input handling and add billing methods
    Refactor user input handling and add billing methods.

--- Assignment5Second.java
+++ Assignment5Second.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 
 public class Main {
 	static Scanner input = new Scanner(System.in);
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //start of main method
+		
 		char userInput;
 		char userChoice;
 		
 		System.out.print("Enter if business or residential: ");
-		userInput = input.next().charAt(0); //will it work with lowerCase?
+		userInput = input.next().toLowerCase().charAt(0);
 		
-		
+	while (userInput = 'r' || userInput =='b') {
 	if (userInput == 'r' || userInput == 'b')  {
 		System.out.println("this is the methid called");
 		//this is where the options and calculations will go once bill and calculate amount will go
 		
 		System.out.println("Please select an option: ");
 		menuDisplay(userChoice = input.next().toLowerCase().charAt(0));
-	
+	//Average cost per day (30 billing days) - it could be either method or on main
+	//Mark's part of the code?? 
+		double amountDue = 0;//Get Nums
+		System.out.printf("Amount due = $%.2f", amountDue);
 		
 		
 		
@@ -28,7 +33,25 @@ public class Main {
 	}
 	
 	
+		} //end of while
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//start of menuOption()
 	public static char menuDisplay(char userInput) {
 		char i;
@@ -38,9 +61,10 @@ public class Main {
 		System.out.print("V - view change in consumption: Compare your current bill with your\nprevious one\n");
 		System.out.print("E - Exit");
 		
-		while (userInput == 'i' || userInput == 'v') {
+		while (userInput == 'i') {
 			if (userInput == 'i') {
-				System.out.print("You are enrolling into our monthly installment plan\n");
+				System.out.println("************************************");
+				System.out.print("If you do not want to make a onetime payment, we have an easy\ninstallment plan for you. This is an ineterest charge plan.\nSign up for the Installment Payment Plan (y/n) ?\n");
 				//Call marks or lins method, opens door to method
 				
 				
@@ -64,4 +88,52 @@ public class Main {
 		}
 		return userInput;
 	}
+	
+	// residentialBill() method - lin's code: 
+	public static double ResidentialBill ()
+	{
+		double KWH;
+		double fee;
+		
+		System.out.print("Welcome Residential Customer,\n Enter the total usage KiloWatt Hours (KWH): \n");
+		KWH = input.nextDouble();
+		
+		fee = KWH * 0.12 + 8.07;
+		fee = fee + fee * 0.05;
+		
+		return fee;
+	}
+	
+	//businessBill() method - Lin's code:
+	public static double BusinessBill ()
+	{
+		double KWH;
+		double fee;
+		int  premiumChannels;
+		
+		System.out.print("Welcome Business Customer,\n Enter the total usage KiloWatt Hours (KWH): \n");
+		KWH = input.nextDouble();
+		System.out.print("Enter the number of premium channels used: \n");
+		premiumChannels = input.nextInt();
+		
+		if (KWH <= 1200)
+		{
+			fee = KWH * 0.17 + 47 * premiumChannels + 17.07;
+		}
+		
+		else
+		{
+			fee = 204 + (KWH-1200) * 0.12 + 47 * premiumChannels + 17.07;
+		}
+		fee = fee + fee * 0.08;
+		
+		
+		return fee;
+	}
+	
+	
+	
+	
+	
+	
 }
