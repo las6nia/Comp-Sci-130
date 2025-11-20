c12fc97976fd92c93ac547ff0fd056d10152bc7b
Luis Alcala <lalcalaroblero2@gmail.com>
2025-11-20 12:46:05 -0600

    Create Class21 with main method and comments
    Initial implementation of Class21 with main method and comments.

---
+++ java/Class21.java
@@ -0,0 +1,23 @@
+package lecture21;
+import java.util.Scanner;
+
+public class Class21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+													//Class lecture - Parallel Arrays
+		//Two or more arrays that have the same length (a must)
+		//What is the point? The elements @ index i of 1st 2nd, and 3rd array, belong to a single record - the value of a 
+		
+		//example - object oriented?? the PLU, itemName, and Price - parallel arrays - 
+		
+		
+		
+															//Second project - Data arrays - student management system
+		int [] names =new int {19,20,18,21,19,}
+		
+		
+
+	}
+
+}
