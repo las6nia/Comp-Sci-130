ea99b5c5a03683fea6ced1cff38464bc5056c231
Luis Alcala <lalcalaroblero2@gmail.com>
2025-12-08 20:50:57 -0600

    Create main class for assignment 3
    Initial implementation of the main class for assignment 3.

---
+++ Assignments/assignment3.java
@@ -0,0 +1,31 @@
+package assignment3;
+import java.util.Scanner;
+public class main {
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String itemName; //self explanatory :D
+		int pOrdered; //number of pieces ordered
+		int pInStore; //number of pieces currently in the store
+		int pSold; //number of pieces sold
+		double manufPrice; //manufacturer’s price for the item
+		double sellingPrice; //store’s selling price
+		
+				//Needs the following:
+		//menu display method - "Your program must be command driven, giving the user various choices, such as adding an item......"
+		//adding an item method()
+		//checking if item is in-store
+		//selling item
+		//printing report - image at the end of week???
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
