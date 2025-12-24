337092bbd9005d7c53187839c7340591ff5b2f7f
Luis Alcala <lalcalaroblero2@gmail.com>
2025-12-24 11:50:02 -0600

    Update LogInSys.java

---
+++ PersonalPractice/LogInSys.java
@@ -0,0 +1,31 @@
+package bankAccountProgram;
+import java.util.Scanner;
+import java.util.Timer;
+public class Main {
+	public static Timer timer = new Timer();
+	public static Scanner input = new Scanner(System.in); //public scanner
+
+	public static void main(String[] args) {
+String passWord;
+
+boolean active = true;
+Boolean newUser;
+Boolean alreadyMember;
+while (active){
+system.out.print("existing user? ");
+    newUser = input.next();
+
+
+
+    
+if (
+
+
+
+system.out.println("Enter your username: ");
+passWord = input.next();
+
+}
+
+}
+}
